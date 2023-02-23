57ae3be80768f65ea40c2dbbb15f990e2d4c420b
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-02-23 04:37:59 +0200

    .
    .

--- javascript/home.js
+++ javascript/home.js
@@ -4,6 +4,34 @@ let modulLogin =document.querySelector(".modul-login")
 let loginContent =document.querySelector('.login-content')
 let postionX = document.querySelector(".postionX")
 let close =document.getElementById("close")
+let loginBtn  = document.getElementById("loginBtn")
+let dat =[]
+function failLogin(){
+  let login =document.querySelector('.sucessLofgin')
+  let h4 = login.querySelector('h4')
+  h4.innerText =`you have no account try register`
+  h4.style.color ='red'
+  login.classList.add('msg')
+  setTimeout(function(){
+    login.classList.remove('msg')
+  
+  },4000)
+
+
+}
+function successLogin(){
+
+let login =document.querySelector('.sucessLofgin')
+login.classList.add('msg')
+let h4 = login.querySelector('h4')
+h4.innerText =`you have login successfully`
+h4.style.color ='green'
+setTimeout(function(){
+  login.classList.remove('msg')
+
+},4000)
+
+}
 bars.addEventListener("click" ,  () =>{
 
 let nav =document.querySelector(".nav-buttons")
@@ -85,3 +113,57 @@ close.addEventListener('click' , () =>{
 
 
 })
+// start login in 
+
+loginBtn.addEventListener('click' , () =>{
+ let input  = document.getElementById('user-name')
+let pass = document.querySelector('.pass')
+
+ if(input.value == '' || pass.value ==''){
+let msg = document.querySelector('.messageForValidating')
+msg.classList.add('showMessage')
+setTimeout(function (){
+  msg.classList.remove('showMessage')
+
+},3000)
+input.style.outline ='2px solid rgb(206, 112, 112)'
+pass.style.outline ='2px solid rgb(206, 112, 112)'
+
+ }else{
+  input.style.outline ='2px solid green'
+pass.style.outline ='2px solid green'
+ const user = {
+  "username" : input.value,
+  "password" : pass.value
+ }
+
+
+axios.post(`https://tarmeezacademy.com/api/v1
+/login` ,user)
+.then(res  =>{
+  const token = res.data.token
+  const user  = res.data.user
+ dat =[ token ,user] 
+console.log(dat)
+localStorage.setItem('userData' , JSON.stringify(dat))
+
+
+})
+
+// ma;e nav bar like login out
+
+ }
+ checkUser()
+})
+//check user if exist or not
+function checkUser (){
+
+  let check =localStorage.getItem('userData')
+  if(check){
+    successLogin()
+    console.log(check)
+  }else{
+failLogin()
+  }
+}
+
